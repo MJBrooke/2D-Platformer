12618043688b967e70a4282636cc18bbd21eb8a1
Michael Brooke <michaelbrooke93@gmail.com>
2024-09-29 20:20:33 +0200

    Add climbing animation to ladders

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,8 +4,9 @@ using UnityEngine.Serialization;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private const float DefaultGravity = 7f;
     private static readonly int IsRunningParam = Animator.StringToHash("isRunning");
+    private static readonly int IsClimbingParam = Animator.StringToHash("isClimbing");
+    private const float DefaultGravity = 7f;
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Animator myAnimator;
@@ -36,11 +37,6 @@ public class PlayerMovement : MonoBehaviour
         _moveInput = value.Get<Vector2>();
     }
 
-    // It is not recommended to make float comparisons against 0, even though the Input System gives a 0 when not moving.
-    // This function creates a 'safe' comparison by taking the positive value of the movement and comparing it to Epsilon, which is a tiny value.
-    // This overcomes floating-point inaccuracies.
-    private bool IsRunning() => Mathf.Abs(_moveInput.x) > Mathf.Epsilon;
-
     private void Run()
     {
         // We set the x-value to our movement speed. If input is non-zero, this creates horizontal movement.
@@ -48,13 +44,13 @@ public class PlayerMovement : MonoBehaviour
         rb.velocity = new Vector2(_moveInput.x * movementSpeed, rb.velocity.y);
 
         // Set the animator's isRunning parameter so that the AnimationController knows to shift from PlayerIdling to PlayerRunning and back.
-        myAnimator.SetBool(IsRunningParam, IsRunning());
+        myAnimator.SetBool(IsRunningParam, IsMovingOnAxis(_moveInput.x));
     }
 
     private void FlipSprite()
     {
         // If moving (i.e. input.x != 0), use the Scale.x value of the Transform component as either -1 or 1 to flip the direction
-        if (IsRunning())
+        if (IsMovingOnAxis(_moveInput.x))
             transform.localScale = new Vector2(Mathf.Sign(_moveInput.x), transform.localScale.y);
     }
 
@@ -70,9 +66,19 @@ public class PlayerMovement : MonoBehaviour
     {
         if (myCollider.IsTouchingLayers(LayerMask.GetMask("Climbing")))
         {
-            rb.gravityScale = 0f;
+            rb.gravityScale = 0f; // Prevent player from 'sinking' on the ladder if not moving
             rb.velocity = new Vector2(rb.velocity.x, _moveInput.y * climbSpeed);
+            myAnimator.SetBool(IsClimbingParam, IsMovingOnAxis(_moveInput.y)); // Only play animation if we're on the ladder and moving
         }
-        else rb.gravityScale = DefaultGravity;
+        else
+        {
+            rb.gravityScale = DefaultGravity;
+            myAnimator.SetBool(IsClimbingParam, false);
+        }
     }
+    
+    // It is not recommended to make float comparisons against 0, even though the Input System gives a 0 when not moving.
+    // This function creates a 'safe' comparison by taking the positive value of the movement and comparing it to Epsilon, which is a tiny value.
+    // This overcomes floating-point inaccuracies.
+    private static bool IsMovingOnAxis(float value) => Mathf.Abs(value) > Mathf.Epsilon;
 }
