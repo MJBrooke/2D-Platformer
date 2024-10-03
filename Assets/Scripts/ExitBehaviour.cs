c7f417345f6c7ed66f94a53cd08b8ca136b04ebc
Michael Brooke <michaelbrooke93@gmail.com>
2024-10-03 22:04:18 +0200

    Add particle effects when the exit sign is touched

--- Assets/Scripts/ExitBehaviour.cs
+++ Assets/Scripts/ExitBehaviour.cs
@@ -3,10 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class ExitBehaviour : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem exitParticles;
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.gameObject.CompareTag("Player")) return;
 
+        // TODO - add some sound
+        exitParticles.Play();
         Invoke(nameof(LoadNextScene), 1f);
     }
 
