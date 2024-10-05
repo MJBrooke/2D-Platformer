63d1b3af47f767f5c00d4e363e1f124431104703
Michael Brooke <michaelbrooke93@gmail.com>
2024-10-05 17:51:54 +0200

    Add sound effect when a coin is picked up

--- Assets/Scripts/CoinBehaviour.cs
+++ Assets/Scripts/CoinBehaviour.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class CoinBehaviour : MonoBehaviour
 {
+    [SerializeField] private AudioClip pickupSound;
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.CompareTag("Player")) return;
         
+        AudioSource.PlayClipAtPoint(pickupSound, transform.position);
         Destroy(gameObject);
     }
 }
