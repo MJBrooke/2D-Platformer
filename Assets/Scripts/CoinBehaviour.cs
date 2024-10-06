c046463d8e0eef98ca554ccf3d1be573abeb9cb4
Michael Brooke <michaelbrooke93@gmail.com>
2024-10-06 21:22:10 +0200

    Play Coin Pickup SFX at half-volume

--- Assets/Scripts/CoinBehaviour.cs
+++ Assets/Scripts/CoinBehaviour.cs
@@ -10,7 +10,7 @@ public class CoinBehaviour : MonoBehaviour
 
         // If you use an AudioSource component attached to the coin here, it would be destroyed before the sound is played.
         // This simple (but somewhat dirty) solution is to play a sound at this location in the Scene, not tied to the coin.
-        AudioSource.PlayClipAtPoint(pickupSound, transform.position);
+        AudioSource.PlayClipAtPoint(pickupSound, transform.position, 0.5f);
 
         GameManager.Instance.AddScore();
 
